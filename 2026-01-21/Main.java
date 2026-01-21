474db12a67596439da3274f0db61a9bd96a163ed
ymin03 <yoonmin5658@gmail.com>
2026-01-21 22:48:45 +0900

    Add files via upload

--- 2026-01-21/Main.java
+++ 2026-01-21/Main.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("이름을 입력하세요: ");
+        String name = sc.nextLine();
+        System.out.print("나이를 입력하세요: ");
+        String ageStr = sc.nextLine();
+        int age = Integer.parseInt(ageStr);
+        System.out.print("키를 입력하세요: ");
+        String heightStr = sc.nextLine();
+        double height = Double.parseDouble(heightStr);
+
+        System.out.println("이름 : "+ name + ", 나이 : " + age + ", 키: " + height);
+
+        if (age >= 20 && height >= 170) {
+            System.out.println("성인입니다.");
+            System.out.println("키가 크시네요!");
+        } else {
+            System.out.println("조건 미달입니다..");
+        }
+    }
+}
